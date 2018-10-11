5468e3397de510e7882bab3c0bf3139e035510b7
C0deboy <ellucky4@gmail.com>
2018-10-12 00:56:12 +0200

    updated dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {StepifyModule} from './stepify/stepify.module';
+import {environment} from './environments/environment';
 
-import { StepifyModule } from './stepify/stepify.module';
-import { environment } from './environments/environment';
+import 'hammerjs';
+
 declare var $: any;
 
 if (environment.production) {
